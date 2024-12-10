2d8711757278a7431961a7148721c9a2b14d71b2
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2024-12-10 18:13:59 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Todos } from "./types/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [todos, setTodos] = useState<Todos[]>([
+    { id: 1, title: "Learn React", completed: false },
+    { id: 2, title: "TypeScript", completed: false },
+  ]);
+  return <div></div>;
+};
 
 export default App;
